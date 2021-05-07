11593923233b0c2bcca39675b0ef25e8e3e1882c
Shusho76 <shusho.an76@gmail.com>
2021-05-08 01:11:08 +0400

    Make dictionary without function

--- dictionary.py
+++ dictionary.py
@@ -2,15 +2,9 @@ class StringToDictionary:
     def __init__(self, name, string):
         self.name = name
         self.str1 = string
-
-
-
-    def dictionary(self):
-        string1 =self.str1 
-        my_dict = {}
-        for letter in self.str1:
-            my_dict[letter] = my_dict.get(letter, 0) + 1
-        return  F"{self.name} of string is {my_dict}"
+        self.dict = {}
+        for key in self.str1:
+        	self.dict[key] = 1
+        print(F"{self.name} of string is {self.dict}") 
 
 dictionary = StringToDictionary("Dictionary",input("Wright your text here\n"))
-print(dictionary.dictionary())
